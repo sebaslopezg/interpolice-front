e30eb3461428f0c6cbea9c931b7e402093509ad6
Sebas <sebaslg96@gmail.com>
2024-12-09 21:45:57 -0500

    actualizacion en delitos

--- assets/js/delitos.js
+++ assets/js/delitos.js
@@ -1,6 +1,42 @@
 const tabla = document.querySelector('#delitos')
+const btnNuevoDelito = document.querySelector('#btnNuevoDelito')
+const txtNombreDelito = document.querySelector('#txtNombreDelito')
+const txtDescripcionDelito = document.querySelector('#txtDescripcionDelito')
+const gradoDelito = document.querySelector('#gradoDelito')
+const btnRegistrar = document.querySelector('#btnRegistrar')
+const idDelito = document.querySelector('#idDelito')
 
 fntListar()
+
+window.addEventListener('submit', (e)=>e.preventDefault())
+
+btnNuevoDelito.addEventListener('click', ()=>{
+    $('#registroModal').modal('show')
+    fntClearForm()
+})
+
+btnRegistrar.addEventListener('click', ()=>{
+    fntRegistrar()
+})
+
+document.addEventListener('click', (e)=>{
+    try {
+        let btn = e.target.closest("button")
+        let accion = btn.getAttribute("data-action")
+        let delitoId = btn.getAttribute("data-delito-id")
+
+        if (accion == 'delete') {
+            fntEliminar(delitoId)
+            fntListar()
+        }
+        
+        if (accion == 'update') {
+            fntEdit(delitoId)
+            $('#registroModal').modal('show')
+        }
+
+    } catch (error) {}
+})
 
 function fntListar(){
 
@@ -18,12 +54,70 @@ function fntListar(){
                 <td>${el.descripcion_delito}</td>
                 <td>${el.grados_delitos_id_grado_delito}</td>
                 <td>
-                    <button class="btn btn-danger" data-action="delete" data-ciudadano-id="${el.id_ciudadanos}"><i class="bi bi-trash"></i></button>
-                    <button class="btn btn-primary" data-action="update" data-ciudadano-id="${el.id_ciudadanos}"><i class="bi bi-pencil-square"></i></button>
+                    <button class="btn btn-danger" data-action="delete" data-delito-id="${el.id_delito}"><i class="bi bi-trash"></i></button>
+                    <button class="btn btn-primary" data-action="update" data-delito-id="${el.id_delito}"><i class="bi bi-pencil-square"></i></button>
                 </td>
             </tr>
             `
         })
         tabla.innerHTML = html
     })
+}
+
+function fntRegistrar(){
+
+    fetch(BASE_URL+'/api/delitos/crear',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+          },
+        body: JSON.stringify({
+            nombre_delito:txtNombreDelito.value,
+            descripcion_delito:txtDescripcionDelito.value,
+            grados_delitos_id_grado_delito:gradoDelito.value
+        })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        Swal.fire({
+            title: data.status ? "Registro insertado" : "Error",
+            text: data.mensaje,
+            icon: data.status ? "success" : "error"
+        })
+        if (data.status) {
+            $('#registroModal').modal('hide')
+            fntListar()
+        }
+    })
+}
+
+function fntEliminar(id){
+
+    fetch(`${BASE_URL}/api/delitos/borrarporid/${id}`,{
+        method: "DELETE"
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data)) 
+}
+
+function fntEdit(id){
+    fetch(`${BASE_URL}/api/delitos/listarporid/${id}`)
+    .then((res) => res.json())
+    .then((json) => {
+        data = json.datos[0]
+        console.log(data)
+        idDelito.value = id_delito
+        txtNombreDelito.value = data.nombre_delito
+        txtDescripcionDelito.value = data.descripcion_delito
+        gradoDelito.value = data.grados_delitos_id_grado_delito
+
+    })
+}
+
+function fntClearForm(){
+    idDelito.value = ""
+    txtNombreDelito.value = ""
+    txtDescripcionDelito.value = ""
+    gradoDelito.value = ""
 }
